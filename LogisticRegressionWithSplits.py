3e792b4aec3a145557db3172ef2eaab0c51f60a6
EG-Miln <findx.hereitis@gmail.com>
2025-06-24 16:06:46 -0400

    Deprecated this logistic regression file in favor of PredictiveModelingCSV

--- LogisticRegressionWithSplits.py
+++ LogisticRegressionWithSplits.py
@@ -18,7 +18,7 @@ feature_cols = ['thirdDownEff','yardsPerPass', 'rushingAttempts', 'turnovers']
 X,y = GetXy(winner_file, loser_file, feature_cols)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #, stratify=y) #, random_state=216)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=216)
 
 
 
@@ -35,12 +35,38 @@ y_pred = model.predict(X_test_scaled)
 y_prob = model.predict_proba(X_test_scaled)[:,1]
 
 # --- EVALUATION
-print("Accuracy:", accuracy_score(y_test, y_pred))
+#print("Accuracy:", accuracy_score(y_test, y_pred))
 #print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 #print("Classification Report:\n", classification_report(y_test, y_pred))
-print("ROC AUC:", roc_auc_score(y_test, y_prob))
+#print("ROC AUC:", roc_auc_score(y_test, y_prob))
 
 # --- FEATURE IMPORTANCE 
+#importances = pd.Series(model.coef_[0], index=feature_cols)
+#print("\n Logistic Regression Coefficients:")
+#print(importances.sort_values(ascending=False))
+
+print("outputting final test results")
+
+df_test = pd.read_csv('NewDataFiles/2024_close_games_team_stats_summary.csv')
+if df_test['Result'].dtype == object:
+    df_test['Result'] = df_test['Result'].map({'winner': 1, 'loser': 0})
+
+X_test = df_test[feature_cols]
+y_test = df_test['Result']
+X_test_scaled = scaler.transform(X_test)
+
+#training data and model are already created above
+
+y_pred = model.predict(X_test_scaled)
+y_prob = model.predict_proba(X_test_scaled)[:,1]
+
+# --- EVALUATION
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("ROC AUC:", roc_auc_score(y_test, y_prob))
+
+
 importances = pd.Series(model.coef_[0], index=feature_cols)
 print("\n Logistic Regression Coefficients:")
 print(importances.sort_values(ascending=False))
