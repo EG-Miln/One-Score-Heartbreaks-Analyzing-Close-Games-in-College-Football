5ea64a9157b6fc8d6d09fced7097c2d34ee8ffd1
EG-Miln <findx.hereitis@gmail.com>
2025-06-24 08:03:05 -0400

    Created data splitting scripts
    Use TrainingDataForSplits.py to split previous seasons dataset
    
    TrainingDataPreparationTestData.py to use 2024 as the test data

---
+++ TrainingDataForSplits.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+    # --- CONVERT possessionTime (e.g. "30:45") TO TOTAL SECONDS (e.g. "1845")
+def time_to_seconds(time_str):
+    try:
+        minutes, seconds = map(int, str(time_str).split(':'))
+        return minutes * 60 + seconds
+    except:
+        return None
+
+def features():
+    #return ['thirdDownEff','completionAttempts', 'yardsPerPass', 'rushingYards', 'possessionTime']
+    return ['firstDowns','thirdDownEff','completionAttempts','yardsPerPass',  'rushingAttempts','turnovers'] #increases accuracy over above for logistic regression
+
+def mergeWinnersLosers(winner_file, loser_file):
+    df_winners = pd.read_csv(winner_file)
+    df_winners['Result'] = 1
+
+    df_losers = pd.read_csv(loser_file)
+    df_losers['Result'] = 0
+
+    df_combined = pd.concat([df_winners, df_losers])
+
+    df_combined['possessionTime'] = df_combined['possessionTime'].apply(time_to_seconds)
+
+    if df_combined['Result'].dtype == object:
+        df_combined['Result'] = df_combined['Result'].map({'winner': 1, 'loser': 0})
+
+    #previous cleaning attempts showed that there are no missing values to handle
+    feature_col = features()
+    for col in feature_col:
+        median_train = df_combined[col].median()
+        df_combined[col] = df_combined[col].fillna(median_train)
+
+    return df_combined
+
+def GetXy(winner_file, loser_file, feature_cols):
+    df_combined = mergeWinnersLosers(winner_file, loser_file)
+    x = df_combined[feature_cols]
+    y = df_combined['Result']
+    
+    return (x,y)
---
+++ TrainingDataPreparationTestData.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+
+    # --- CONVERT possessionTime (e.g. "30:45") TO TOTAL SECONDS (e.g. "1845")
+def time_to_seconds(time_str):
+    try:
+        minutes, seconds = map(int, str(time_str).split(':'))
+        return minutes * 60 + seconds
+    except:
+        return None
+
+def features():
+    return ['thirdDownEff', 
+                    'completionAttempts', 
+                    'yardsPerPass', 
+                    'rushingYards', 
+                    'possessionTime']
+
+def GetTrainingData(winner_file, loser_file, test_file):
+
+    # --- LOAD TRAINING DATA (FBS SEASONS FROM 2018, 2019, AND 2021-2023)
+    df_winners = pd.read_csv(winner_file)
+    df_winners['Result'] = 1
+
+    df_losers = pd.read_csv(loser_file)
+    df_losers['Result'] = 0
+
+    df_train = pd.concat([df_winners, df_losers])
+
+
+    # --- LOAD TESTING DATA (2024 FBS SEASON)
+    df_test = pd.read_csv(test_file)
+    # --- MAP THE TESTING RESULT COLUMN (e.g. Winner -> 1 ; Loser -> 0)
+    if df_test['Result'].dtype == object:
+        df_test['Result'] = df_test['Result'].map({'winner': 1, 'loser': 0})
+
+    # --- KEY FEATURES 
+    feature_cols = features()
+
+
+
+    # --- APPLY TIME CONVERSION 
+    df_train['possessionTime'] = df_train['possessionTime'].apply(time_to_seconds)
+    df_test['possessionTime'] = df_test['possessionTime'].apply(time_to_seconds)
+
+
+    #previous cleaning attempts showed that there are no missing values to handle
+    for col in feature_cols:
+        median_train = df_train[col].median()
+        median_test = df_test[col].median()
+        df_train[col] = df_train[col].fillna(median_train)
+        df_test[col] = df_test[col].fillna(median_test)
+
+    # --- SET X AND y 
+    X_train = df_train[feature_cols]
+    y_train = df_train['Result']
+
+    X_test = df_test[feature_cols]
+    y_test = df_test['Result']
+
+    return [X_train, y_train, X_test, y_test]
